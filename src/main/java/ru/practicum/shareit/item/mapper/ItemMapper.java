266082f74effe611c0ae9b1237c12e25e065ddfa
tortiss <tortiss0000@yandex.ru>
2022-08-06 19:03:23 +0400

    feat: Выполнил согластно заданию №13.

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,9 +1,6 @@
 package ru.practicum.shareit.item.mapper;
 
-import org.mapstruct.InjectionStrategy;
-import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
